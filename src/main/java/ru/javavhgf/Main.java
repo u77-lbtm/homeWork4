77f3183dd3c1a8fa849e3657a875ba85e4e5c4c3
main <kraftrr77@gmail.com>
2026-06-08 09:41:44 +0500

    homeWork7

--- src/main/java/ru/javavhgf/Main.java
+++ src/main/java/ru/javavhgf/Main.java
@@ -1,13 +1,16 @@
 package ru.javavhgf;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello!");
+        // Задача 1
+        int clientOS = 1;
 
-        
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        }
+
+
     }
 }
